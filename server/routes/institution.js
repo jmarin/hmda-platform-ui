abefc1eb1fd484724f65e71c49d643322ad583a8
Juan Marin Otero <juan.marin.otero@gmail.com>
2016-06-15 09:31:03 -0400

    Add backend institutions endpoint

--- server/routes/institution.js
+++ server/routes/institution.js
@@ -1,11 +1,19 @@
 var fs = require('fs');
 var router = require('express').Router();
 var periodRouter = require('./period');
+var request = require('request');
 
 var institutionsObj = JSON.parse(fs.readFileSync('./server/json/user1-institutions.json'));
 
 router.get('/', function(req, res){
-  res.send(institutionsObj);
+  //res.send(institutionsObj);
+  console.log(root.path);
+  request.get('http://localhost:8080/institutions', function(err, response, body){
+    if (!err && response.statusCode == 200) {
+      var json = JSON.parse(body);
+      res.send(json);
+    }  
+  }); 
 });
 
 router.get('/:institution', function(req, res){
